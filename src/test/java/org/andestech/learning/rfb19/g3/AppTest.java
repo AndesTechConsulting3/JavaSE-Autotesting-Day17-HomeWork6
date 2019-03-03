783cc553d5012bc7c966ab0dcaf6aa4e3bf4040b
AntonB <mortician233@gmail.com>
2019-03-03 11:48:25 +0300

    1

--- src/test/java/org/andestech/learning/rfb19/g3/AppTest.java
+++ src/test/java/org/andestech/learning/rfb19/g3/AppTest.java
@@ -1,11 +1,15 @@
 package org.andestech.learning.rfb19.g3;
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 import static org.testng.Assert.assertTrue;
 
 public class AppTest
@@ -15,7 +19,7 @@ public class AppTest
     @BeforeClass
     public void initData(){
     System.setProperty("webdriver.chrome.driver",
-            "E:\\drivers\\selenium\\chromedriver.exe");
+            "lib/chromedriver.exe");
     System.out.println("+++ Class: " + this);
 
     }
@@ -24,10 +28,91 @@ public class AppTest
     public void testCaseChrome01()
     {
         wd = new ChromeDriver();
-        wd.get("http://lenta.ru");
-        assertTrue( true );
+        wd.get("http://andestech.org/learning/rfb18/index.html");
+        wd.manage().timeouts().pageLoadTimeout(3,TimeUnit.SECONDS);
+        wd.findElement(By.linkText("New customer")).isEnabled();
     }
 
+    @Test
+    public void testCaseChrome02()
+    {
+        wd = new ChromeDriver();
+        wd.get("http://andestech.org/learning/rfb18/index.html");
+        wd.manage().timeouts().pageLoadTimeout(3,TimeUnit.SECONDS);
+        wd.findElement(By.linkText("New customer")).isEnabled();
+        wd.findElement(By.linkText("New customer")).click();
+        wd.findElement(By.name("reset")).click();
+        wd.findElement(By.cssSelector("input#name")).sendKeys("Name");
+        wd.findElement(By.cssSelector("input#sname")).sendKeys("SName");
+        wd.findElement(By.cssSelector("input#login")).sendKeys("TestLogin");
+        wd.findElement(By.cssSelector("input#pass")).sendKeys("Test12!!!");
+        wd.findElement(By.name("submit")).submit();
+        wd.switchTo().alert().accept();
+    }
+
+    @Test
+    public void testCaseChrome03()
+    {
+        wd = new ChromeDriver();
+        wd.get("http://andestech.org/learning/rfb18/index.html");
+        wd.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        wd.findElement(By.linkText("New customer")).isEnabled();
+        wd.findElement(By.linkText("New customer")).click();
+        wd.findElement(By.name("reset")).click();
+        wd.findElement(By.cssSelector("input#name")).sendKeys("Тест");
+        wd.findElement(By.cssSelector("input#sname")).sendKeys("CТест");
+        wd.findElement(By.cssSelector("input#login")).sendKeys("TestLogin");
+        wd.findElement(By.cssSelector("input#pass")).sendKeys("Test12!!!");
+        wd.findElement(By.name("submit")).submit();
+        wd.switchTo().alert().accept();
+        wd.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        wd.findElement(By.linkText("Login")).click();
+        wd.findElement(By.name("reset")).click();
+        wd.findElement(By.cssSelector("input#login")).sendKeys("TestLogin");
+        wd.findElement(By.cssSelector("input#pass")).sendKeys("Test12!!!");
+        wd.findElement(By.name("submit")).submit();
+    }
+
+    @Test
+public void testCaseChrome04()
+{
+    wd = new ChromeDriver();
+    wd.get("http://andestech.org/learning/rfb18/index.html");
+    wd.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+    wd.findElement(By.linkText("New customer")).isEnabled();
+    wd.findElement(By.linkText("New customer")).click();
+    wd.findElement(By.name("reset")).click();
+    wd.findElement(By.cssSelector("input#name")).sendKeys("Тест");
+    wd.findElement(By.cssSelector("input#sname")).sendKeys("CТест");
+    wd.findElement(By.cssSelector("input#login")).sendKeys("TestLogin");
+    wd.findElement(By.cssSelector("input#pass")).sendKeys("Test12!!!");
+    wd.findElement(By.name("submit")).submit();
+    wd.switchTo().alert().accept();
+    wd.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+    wd.findElement(By.linkText("Login")).click();
+    wd.findElement(By.name("reset")).click();
+    wd.findElement(By.cssSelector("input#login")).sendKeys("TestLogin");
+    wd.findElement(By.cssSelector("input#pass")).sendKeys("Test12!!!");
+    wd.findElement(By.name("submit")).submit();
+    wd.getTitle().compareTo("Вы зашли как TestLogin.");
+    wd.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+    wd.findElement(By.cssSelector("#sub > footer > nav > div > a:nth-child(5)")).click();
+    wd.switchTo().alert().accept();
+}
+
+    @Test
+    public void testCaseChrome05()
+    {
+        wd = new ChromeDriver();
+        wd.get("http://andestech.org/learning/rfb18/index.html");
+        wd.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        wd.findElement(By.linkText("Login")).click();
+        wd.findElement(By.name("reset")).click();
+        wd.findElement(By.cssSelector("input#login")).sendKeys("TestLogin");
+        wd.findElement(By.cssSelector("input#pass")).sendKeys("Test12!!!");
+        wd.findElement(By.name("submit")).submit();
+        wd.findElement(By.linkText("Login")).isEnabled();
+    }
 
     @AfterClass
     public void tearDown()
