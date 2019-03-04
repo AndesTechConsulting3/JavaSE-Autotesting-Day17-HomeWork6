d0fccc222ccce1783d6ef3b6f830fb66b39edb4e
Zolin Aleksandr <Aleksandr.ZOLIN@raiffeisen.ru>
2019-03-04 18:03:37 +0300

    commit3

--- src/test/java/org/andestech/learning/rfb19/g3/AppTest.java
+++ src/test/java/org/andestech/learning/rfb19/g3/AppTest.java
@@ -1,6 +1,7 @@
 package org.andestech.learning.rfb19.g3;
 
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,10 +10,15 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -23,32 +29,33 @@ public class AppTest
 {
     private WebDriver wd = null;
 
-    @BeforeClass
-    public void initData(){
-    System.setProperty("webdriver.chrome.driver",
-            "C:\\WebDrivers\\chromedriver.exe");
-    System.out.println("+++ Class: " + this);
+    @BeforeTest
+    public void initData() {
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\WebDrivers\\chromedriver.exe");
+        System.out.println("+++ Class: " + this);
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--start-maximized");
+
+        wd = new ChromeDriver(chromeOptions);
+        wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        wd.get("http://andestech.org/learning/rfb18/");
 
     }
 
     @Test()
     public void testCheckUrl() throws InterruptedException {
 
-        ChromeOptions chromeOptions = new ChromeOptions();
-        //FirefoxOptions firefoxOptions = new FirefoxOptions();
-        chromeOptions.addArguments("--start-maximized");
-
-        wd = new ChromeDriver(chromeOptions);
-        wd.get("http://andestech.org/learning/rfb18/");
-        wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        assertEquals(wd.getCurrentUrl(), "http://andestech.org/learning/rfb18/");
+        WebElement element1 = wd.findElement(By.id("mainTitle"));
+        assertEquals(element1.getText(), "Это тестовый сайт.");
+        tearDown();
     }
 
     @Test(dependsOnMethods = "testCheckUrl")
     public void registrationTest() throws InterruptedException {
 
         ChromeOptions chromeOptions = new ChromeOptions();
-        //FirefoxOptions firefoxOptions = new FirefoxOptions();
         chromeOptions.addArguments("--start-maximized");
 
         wd = new ChromeDriver(chromeOptions);
@@ -62,19 +69,25 @@ public class AppTest
         element2.sendKeys("Александр");
 
         WebElement element3 = wd.findElement(By.id("sname"));
-        element3.sendKeys("Солнышко" + new Random().nextInt(1000));
+        element3.sendKeys("Солнышко");
 
         WebElement element4 = wd.findElement(By.id("login"));
-        element4.sendKeys("WildBanana" + new Random().nextInt(1000));
+        String userLogin = "WildBanana" + new Random().nextInt(1000);
+        element4.sendKeys(userLogin);
 
-        WebElement element5 = wd.findElement(By.name("pass"));
+        WebElement element5 = wd.findElement(By.id("pass"));
         element5.sendKeys("NewPass1");
 
         WebElement element6 = wd.findElement(By.name("submit"));
         element6.click();
-//login button
-        //WebElement element4 = wd.findElement(By.name("submit")); -- y
 
+        Alert alert = wd.switchTo().alert();
+        String alertText = alert.getText();
+        System.out.println(alertText);
+        assertEquals("User  "+ userLogin +" created ok!", alertText);
+        alert.accept();
+
+        tearDown();
 
     }
 
@@ -82,7 +95,6 @@ public class AppTest
     public void testLogin() throws InterruptedException {
 
         ChromeOptions chromeOptions = new ChromeOptions();
-        //FirefoxOptions firefoxOptions = new FirefoxOptions();
         chromeOptions.addArguments("--start-maximized");
 
         wd = new ChromeDriver(chromeOptions);
@@ -104,6 +116,7 @@ public class AppTest
         element4.click();
 
         assertEquals(wd.getCurrentUrl(), "http://andestech.org/learning/rfb18/home.html");
+        tearDown();
     }
 
     @Test(dependsOnMethods = "testLogin")
@@ -135,6 +148,7 @@ public class AppTest
 
         WebElement element5 = wd.findElement(By.cssSelector("a:nth-child(5)"));
         element5.click();
+        tearDown();
 
     }
 
@@ -153,7 +167,7 @@ public class AppTest
         element1.click();
 //insert logopass
         WebElement element2 = wd.findElement(By.name("login"));
-        element2.sendKeys("WildBanana");
+        element2.sendKeys("RANDOMUSER");
 
         WebElement element3 = wd.findElement(By.name("password"));
         element3.sendKeys("NewPass1");
@@ -163,11 +177,12 @@ public class AppTest
         WebElement element4 = wd.findElement(By.cssSelector("#lgn"));
         element4.click();
 
-        assertEquals(wd.getCurrentUrl(), "http://andestech.org/learning/rfb18/home.html");
+        assertEquals(wd.getCurrentUrl(), "http://andestech.org/learning/rfb18/login.html"); //почему то ничего не происходит по клику - сбрасываются значения логопаса
+        tearDown();
+
     }
 
 
-    @AfterClass
     public void tearDown()
     {
       if(wd != null) wd.quit();
